e9051382b2d454e0841b0b896159a5250019a013
euclidstellar <euclidstellar@gmail.com>
2025-06-17 15:21:44 +0530

    js and go test

--- js-code/main.js
+++
@@ -1,6 +0,0 @@
-function greet(name) {
-  console.log("Hello, " + name) // Missing semicolon, if "semi" rule is "error"
-  var unusedVariable; // Will trigger no-unused-vars
-}
-
-greet("World") // Missing semicolon
